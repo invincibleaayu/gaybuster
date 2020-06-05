1633edbc5182a1be8cb65f757a2640f8f7efe6f6
Aayush Dip Giri <invincibleaayu@.github.com>
2020-06-05 08:47:35 +0545

    this commit is final

--- gaybuster.py
+++ gaybuster.py
@@ -3,26 +3,23 @@ import cv2
 import glob
 import face_recognition
 from PIL import ImageDraw
+import numpy as np
 
 
 
 video_capture=cv2.VideoCapture(0)
-guy_face_encoding=[]
-gay_face_encoding=[]
-gay_images=glob.glob("C:/Users/Dell/Desktop/gaubuster/images/gay_images/*.jpg")
-guy_images=glob.glob("C:/Users/Dell/Desktop/gaubuster/images/guy_images/*.jpg")
+# Load a sample picture and learn how to recognize it.
+guy_image = face_recognition.load_image_file("C:/Users/Dell/Desktop/gaubuster/images/guy_images/aayush.jpg")
+guy_face_encoding = face_recognition.face_encodings(guy_image)[0]
 
-for guy_image in guy_images:
-    guy_face=face_recognition.load_image_file(guy_image)
-    encoding=face_recognition.face_encodings(guy_face)[0]
-    guy_face_encoding.append(encoding)
-for gay_image in gay_images:
-    gay_face=face_recognition.load_image_file(gay_image)
-    encoding=face_recognition.face_encodings(gay_face)[0]
-    gay_face_encoding.append(encoding)
 
-known_face_encoding=[
-    guy_face_encoding+gay_face_encoding
+# Load a second sample picture and learn how to recognize it.
+gay_image = face_recognition.load_image_file("C:/Users/Dell/Desktop/gaubuster/images/gay_images/gay2.jpg")
+gay_face_encoding = face_recognition.face_encodings(gay_image)[0]
+
+known_face_encoding = [
+    guy_face_encoding,
+    gay_face_encoding
 ]
 known_names=[
     "guy",
@@ -45,6 +42,40 @@ while True:
         face_encodings=face_recognition.face_encodings(rgb_small_frame,face_locations)
         face_names=[]
         for face_encoding in face_encodings:
-            matches=face_recognition.compare_faces(face_encoding,known_face_encoding)
-            name="unknown"
+            matches=face_recognition.compare_faces(known_face_encoding,face_encoding)
+            name="gay"
+            if True in matches:
+                print(matches)
+                first_match_index=matches.index(True)
+                name = known_names[first_match_index]
+            face_names.append(name)
+            
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255,0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) &  0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
 video_capture.release()
+cv2.destroyAllWindows()
